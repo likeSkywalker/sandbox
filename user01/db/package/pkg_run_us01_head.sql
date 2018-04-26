ae0466c124c7a6dc5e2bf85734934023ab250048
Oleksii_Havryliuk <Oleksii_Havryliuk@epam.com>
2018-04-26 16:54:24 +0200

    add sql for user01

---
+++ user01/db/package/pkg_run_us01_head.sql
@@ -0,0 +1,9 @@
+create or replace PACKAGE PKG_RUN_US01 AS 
+
+  /* TODO enter package declarations (types, exceptions, methods etc) here */ 
+  procedure p_insert_result (ip_count in number);
+  
+  procedure p_clean_result;
+
+END PKG_RUN_US01;
+/
