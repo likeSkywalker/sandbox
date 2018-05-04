0ae21501533c28f5ab02a7750a9f18331a02af18
Oleksii_Havryliuk <Oleksii_Havryliuk@epam.com>
2018-05-04 10:10:38 +0200

    user02 - v3

--- user02/db/package/pkg_run_us02_head.sql
+++ user02/db/package/pkg_run_us02_head.sql
@@ -3,7 +3,7 @@ PROMPT ** Create package head PKG_RUN_US02 **
 create or replace PACKAGE PKG_RUN_US02 AS 
 
   /* This comment should be added before merge to env/int01
-    Version 1.02  */ 
+    Version 1.03  */ 
   
   procedure p_insert_result (ip_count in number);
   
