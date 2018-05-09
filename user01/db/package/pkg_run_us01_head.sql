15809649c0150402e90686f013c7ef0dc038b330
Oleksii_Havryliuk <Oleksii_Havryliuk@epam.com>
2018-05-09 09:10:08 +0200

    lets try

--- user01/db/package/pkg_run_us01_head.sql
+++ user01/db/package/pkg_run_us01_head.sql
@@ -3,7 +3,7 @@
 create or replace PACKAGE PKG_RUN_US01 AS 
 
   /* This comment should be added before merge to env/int01
-    Version 1.27  */ 
+    Version 1.28  */ 
   
   procedure p_insert_result (ip_count in number);
   
