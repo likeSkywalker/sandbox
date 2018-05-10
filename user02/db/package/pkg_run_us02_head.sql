6548c57563b268d13c82badeb009281ff4b20658
Oleksii_Havryliuk <Oleksii_Havryliuk@epam.com>
2018-05-10 14:54:44 +0200

    error

--- user02/db/package/pkg_run_us02_head.sql
+++ user02/db/package/pkg_run_us02_head.sql
@@ -5,7 +5,7 @@ create or replace PACKAGE PKG_RUN_US02 AS
   /* This comment should be added before merge to env/int01
     Version 1.18  */ 
   
-  procedure p_insert_result (ip_count in number);
+  procedure p_insert_result (ip_count in numberok);
   
   procedure p_clean_result;
 
