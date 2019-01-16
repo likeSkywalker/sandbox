6cc0fca6b79a290cb29e0a1fee9308e579ff4728
OrientDreams <38911237+OrientDreams@users.noreply.github.com>
2019-01-16 14:54:18 +0100

    change version

--- user01/db/procedure/insert_result_us01.sql
+++ user01/db/procedure/insert_result_us01.sql
@@ -1,7 +1,7 @@
 @&print -c 'Create procedure INSERT_RESULT_US01'
 
 /* Check codeowners feature
- * Version v07
+ * Version v08
  */
 CREATE OR REPLACE PROCEDURE INSERT_RESULT_US01 (ip_count in number) AS
   pragma autonomous_transaction;
